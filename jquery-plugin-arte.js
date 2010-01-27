2309b3eebf0672e6c7a2e764a991dd4fe84a6107
arthur.obriot <arthur.obriot@b6bd23ea-0a9c-11df-9f86-67d497c0387b>
2010-01-27 13:52:19 +0000

    add the get function add the new functionnality: the auto refresh html element add a test directory associated to this new functionnality

--- jquery-plugin-arte.js
+++ jquery-plugin-arte.js
@@ -14,6 +14,7 @@
 	var _config 	= null;
 	var _is_started = false;
 	var _list_xml_actions = new Array();
+	var _list_html_node = new Array();
 
 	// manage parameters
 	// this function test if parameters are not already initialized
@@ -76,6 +77,17 @@
 		return this;
 	}
 	
+	// used to get parameter values
+	function _get(key)
+	{
+		if (_config[key])
+			return _config[key];
+		if (key == "nb_node")
+			return _list_xml_actions.length;
+		if (key == "nb_elt")
+			return _list_html_node.length;
+	}
+	
 	// main routine, used to execute one round more
 	function _launch_loop()
 	{
@@ -92,8 +104,15 @@
 			data: 		ajax_data,
 			dataType: 	_config['ajax_type'],
 			success: 	function(data, textStatus){
-				if (_config['on_success'])	_config['on_success'](data);
-				if (_config['ajax_type']) _execute_custom_xml_actions(data);
+				// execute the custom list of action with some xml nodes
+				if (_list_xml_actions.length > 0)
+					_execute_custom_xml_actions(data);
+				// auto refresh the list of html element
+				if (_list_html_node.length > 0)
+					_execute_custom_html_nodes(data);
+				// call the final success function
+				if (_config['on_success'])	
+					_config['on_success'](data);
 			}
 		});
 		
@@ -112,11 +131,29 @@
 			});
 	}
 	
+	// this function, which takes the xml response from the ajax query, parse the response
+	// to set automatically some custom html fields
+	function _execute_custom_html_nodes(data_xml)
+	{
+		for (i = 0; i < _list_html_node.length; i++)
+		{
+			$(data_xml).find(_list_html_node[i].node_name).each(function (){
+				$(_list_html_node[i].elt).text($(this).text());
+			});
+		}
+	}
+	
 	// this function is used to add a custom action to be executed after a success ajax request
 	// 'node_name' is name of the xml node we want to get the value
 	// 'fct' the function which will be called if we find this node
 	function _add_custom_xml_actions(node_name, fct)
 	{
+		// test if the element doesn't already exist.
+		for (i = 0; i < _list_xml_actions.length; i++)
+			if (_list_xml_actions[i].node_name == node_name)
+				return this;
+		
+		// add the element in the tab
 		_list_xml_actions.push(new _XmlItem(node_name, fct));
 		return this;
 	}
@@ -142,22 +179,63 @@
 		
 		// make public links with hidden functions
 		return {
-			start:		_start,
-			is_started:	_is_started,
-			stop:		_stop,
-			toogle:		_toogle,
-			set:		_set,
-			launch:		_launch_loop,
-			add_action:	_add_custom_xml_actions,
-			del_action:	_del_custom_xml_actions
+			start:			_start,
+			is_started:		_is_started,
+			stop:			_stop,
+			toogle:			_toogle,
+			set:			_set,
+			get:			_get,
+			launch:			_launch_loop,
+			add_action:		_add_custom_xml_actions,
+			del_action:		_del_custom_xml_actions
 		};
 	});
 	
+	// this function is used to make an auto update of a custom html field
+	// 'node_name' is name of the xml node we want to use to udpate the html field
+	// if no arguement is passed, stop the auto refresh for these nodes
+	$.fn.arte = (function (node_name)
+	{
+		// parse the list of html node
+		this.each(function() {
+			if (node_name)
+			{
+				// add the element, and test if the element doesn't already exist
+				var is_in = false;
+				for (i = 0; i < _list_html_node.length; i++)
+					if (_list_html_node[i].elt == this)
+						is_in = true;
+				if (is_in == false)
+					_list_html_node.push(new _HtmlAuto(this, node_name));
+			}
+			else
+			{
+				// remove the element
+				var newtab = new Array();
+				for (i = 0; i < _list_html_node.length; i++)
+					if (_list_html_node[i].elt != this)
+						newtab.push(_list_html_node[i]);
+				// update the custom list
+				_list_html_node = newtab;
+			}
+		});
+	
+		return this;
+	});
+	
 	// the following class is just a container to join a string with a function
-	// it will be used for ths xml actions
+	// it will be used for the xml actions
 	function _XmlItem(node_name, fct)
 	{
 		this.node_name = node_name;
 		this.fct = fct;
+	}
+	
+	// the following class is just a container to join an html element with a string
+	// it will be used for the auto update html field 
+	function _HtmlAuto(elt, node_name)
+	{
+		this.elt = elt;
+		this.node_name = node_name;
 	}
 })(jQuery);
